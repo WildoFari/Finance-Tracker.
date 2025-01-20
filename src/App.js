0941588e6ec9289c8c730244c100edefb0185145
WildoDev <wildofari99@gmail.com>
2025-01-19 22:41:36 -0300

    Dashborad add

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 
+
 import './App.css';
 import NavBar from './components/NavBar';
+import Dashboard from './components/Dashboard';
+import NotFound from './components/NotFound';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <NavBar />
+    <BrowserRouter>
+      <NavBar />
+      <Routes>
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/" element={<Dashboard />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
