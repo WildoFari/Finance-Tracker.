97a864394313e4c429d55700b231a4706b734ac5
WildoDev <wildofari99@gmail.com>
2025-01-30 17:27:29 -0300

    Added a button to delete all transactions

--- src/components/TransactionList.js
+++ src/components/TransactionList.js
@@ -2,11 +2,17 @@ import React, { useContext } from 'react';
 import { TransactionContext } from '../context/TransactionContext';
 
 const TransactionList = () => {
-    const { transactions } = useContext(TransactionContext);
+    const { transactions, clearTransactions } = useContext(TransactionContext);
 
     return (
         <div className="my-4">
             <h2>Transacciones</h2>
+            {transactions.length > 0 && (
+                <button className="btn btn-danger mb-3 w-100" onClick={clearTransactions}>
+                    Eliminar Todas las Transacciones
+                </button>
+            )}
+
             <ul className="list-group">
                 {transactions.length > 0 ? (
                     transactions.map((transaction, index) => (
@@ -15,7 +21,9 @@ const TransactionList = () => {
                         </li>
                     ))
                 ) : (
-                    <li className="list-group-item text-center text-muted">No hay transacciones registradas</li>
+                    <li className="list-group-item text-center text-muted">
+                        No hay transacciones registradas
+                    </li>
                 )}
             </ul>
         </div>
