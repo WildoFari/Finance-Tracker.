70cf18c8ba4faf16f506c07d79bd18b75804c52e
WildoDev <wildofari99@gmail.com>
2025-09-18 16:54:45 -0300

    Se actualizó el placeholder del campo de entrada en el componente AddInvestment, cambiando de "Nombre del terreno" a "Nombre" para una mejor claridad y concisión.

--- src/components/AddInvestment.js
+++ src/components/AddInvestment.js
@@ -91,7 +91,7 @@ const AddInvestment = ({ addInvestment, existingInvestments }) => {
             <input
                 type="text"
                 className="form-control mb-2"
-                placeholder="Nombre del terreno"
+                placeholder="Nombre"
                 value={name}
                 onChange={(e) => setName(e.target.value)}
             />
